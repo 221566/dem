27f84edd11dfd3a1ec737d62f8f94500a276399c
221566 <2215668921@qq.com>
2021-08-18 22:58:00 +0800

    修改启动启动文案

--- src/main/java/com/lx/dem/DemApplication.java
+++ src/main/java/com/lx/dem/DemApplication.java
@@ -1,13 +1,19 @@
 package com.lx.dem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemApplication {
-
+     private static final Logger LOG = LoggerFactory.getLogger(DemApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(DemApplication.class, args);
+        SpringApplication app = new SpringApplication(DemApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
